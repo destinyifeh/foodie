861d3607ea7d192b5d02f587050187e1849b596e
Destiny Ifehor <Destiny.Ifehor@interswitchgroup.com>
2023-05-11 14:04:27 +0100

    app updated

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import {STAGING_DEPLOYMENT_KEY} from '@env';
 import React, {useEffect} from 'react';
 import {StatusBar, View} from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
 import codePush from 'react-native-code-push';
-import {CODEPUSH_DEPLOYMENT_KEY} from './src/constants/setup';
 import Routes from './src/routes';
 
 const codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_RESUME};
 
 function App() {
-  //onPress={handle.bind(this, 'desto')}>
-
   useEffect(() => {
     RNBootSplash.hide({fade: true});
     codePush.sync({
       installMode: codePush.InstallMode.IMMEDIATE,
-      deploymentKey: CODEPUSH_DEPLOYMENT_KEY,
     });
-    console.log(STAGING_DEPLOYMENT_KEY, 'STAGG');
-    console.log(CODEPUSH_DEPLOYMENT_KEY, 'read');
   }, []);
   return (
     <View style={{flex: 1}}>
